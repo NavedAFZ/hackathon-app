1ca8b472f6feacd6ad57d4905c1541172c6d35a2
nafzal <nafzal@C02F538KMD6M.local>
2021-04-29 17:24:14 +0530

    devise setup

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,19 @@
+class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+    protect_from_forgery with: :exception
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, 
+        keys: [:employee_id, :email, :password , :password_confirmation])
+      devise_parameter_sanitizer.permit(:sign_in, 
+        keys: [:login, :password , :password_confirmation])
+      devise_parameter_sanitizer.permit(:account_update, 
+        keys: [:employee_id, :email, :password , :password_confirmation, :current_password])
+    end
+
+  
+  
+end
---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  attr_accessor :login
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :o mniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :employee_id, presence: true, length: {maximum:4}       
+
+  def self.find_for_database_authentication warden_condition
+    conditons = warden_condition.dup
+    login = conditons.delete(:login)
+    where(conditons).where(
+      ["lower(employee_id) = :value OR lower(email) = :value",
+      { value: login.strip.downcase}]).first
+  end
+end
+
